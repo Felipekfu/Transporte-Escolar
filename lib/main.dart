2af409b69534c1953e0f65c9b36ba80ef384ac76
Felipe de Godoi <felipegcardozo@gmail.com>
2025-04-24 04:16:21 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Transporte Escolar',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(), // Aqui vamos colocar a tela inicial real depois
+    );
+  }
+}
